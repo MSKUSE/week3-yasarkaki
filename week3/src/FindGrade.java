35210022b61512a4df1c726fa8adcf42c41d1ca0
Yaşar Kaki <yasarkaki@posta.mu.edu.tr>
2025-03-06 12:46:55 +0300

    FindGrade.java is created

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -1,6 +1,29 @@
 public class FindGrade{
 
     public static void main(String[] args){
+        int length_of_args = args.length;
+        if (length_of_args == 0){
+            System.out.println("You should give a score!!!");
+        }else {
+            int grade = Integer.parseInt(args[0]);
+
+            if (grade >= 90){
+                System.out.println("Grade: A");
+            }else if (grade >= 80 && grade <89 ) {
+                System.out.println("Grade: B");
+            }else if (grade >=70 && grade < 79) {
+                System.out.println("Grade: C");
+            }else if (grade >= 60 && grade < 69) {
+                System.out.println("Grade: D");
+            }else if (grade >= 50 && grade <59) {
+                System.out.println("Grade: F");
+            }else{
+                System.out.println("Invalid Score");
+            }
+        }
+
+
+
 
         //Get the score from the user as a file parameter
         //Calculate the grade based on the score
